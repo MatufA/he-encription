3fec942b70c5d629ba5217401b970e9e30308ee3
Adiel Matuf <matufadiel@gmail.com>
2019-09-23 12:31:49 +0300

    remove unuse

--- client-side/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const main = require('./routes/index')
-
-const app = express()
-const port = 3000
-
-app.use('/', main)
-
-app.listen(port, () => console.log(`app listening on port ${port}!`))
